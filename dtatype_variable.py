3955e8ff0e24d45a3e91e50e0125c58b4588cfaf
gamol4856-max <gamol4856@gmail.com>
2025-11-28 13:23:23 +0530

    Add files via upload
    data type practice in python

---
+++ dtatype_variable.py
@@ -0,0 +1,52 @@
+Name ="Amol"
+Age = 32
+city = "pune"
+
+print (Name, Age, city)
+
+
+x = 10
+y = 3.5
+z = "hello"
+
+print (type(x), type(y), type(z))
+
+
+a = 5
+b = 7
+print ("sum =", a + b)
+
+x = 4
+y = 9
+x,y=y,x
+print (x,y)
+
+first = "python"
+second = "programming"
+
+print (first +" "+
+        second)
+
+'''first = "Python"
+second = "Programming"
+
+result = first + " " + second
+print(result)'''
+
+
+a = "20"
+a=int(a)
+print (type(a),a)
+
+
+a = 42
+b = 3.14
+c = "hello"
+d = True
+e = "123"
+f = 9.0
+
+print ("a.integer b. float\n c. string\n d. boolean\n e. string\n f. float\n")
+
+
+
