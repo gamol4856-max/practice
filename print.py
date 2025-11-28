906f7df22b16952e8baddc4ffc6a7de641e6e2e8
gamol4856-max <gamol4856@gmail.com>
2025-11-28 11:58:42 +0530

    Add files via upload
    print.py file have detail print statement practice of 'END' = and 'SEP' = parameters 
    
    string.py file have some basics of string operations

---
+++ print.py
@@ -0,0 +1,52 @@
+#In Python, print() is a built-in function used to output text or other data to the console (standard output).
+
+'''
+✔️ What print() Does?
+*Displays the given values on the screen.
+*Converts non-string values into strings automatically.
+*Adds a newline at the end by default.
+*Allows custom separators and endings.'''
+
+#example
+print("Hello word")
+
+#******************Common Parameters********
+#✔️ Examples
+#eparator between items (default: space " ")
+
+#✅ Using sep (separator)
+#Controls what goes between items.
+
+
+
+print("apple", "banana", "cherry", sep = "0")
+# Output:apple0banana0cherry
+
+
+print("2025", "01", "30", sep="-")
+# Output: 2025-01-30
+
+
+print ("$iva@", "Amol", "2025", sep = "-")
+
+
+print ("A", "B", "C", "D", sep = "-")
+
+#A-B-C-D
+
+#✅ Using end
+
+#Controls what prints at the end (default is newline \n).
+
+'''print("Hello", end=" ")
+
+print("Amol")
+
+print("my fev food is", end = " ")
+
+print ("12345")'''
+
+
+print ("😊", end = "")
+print ("😒", end = "")
+print ("i love you amol💕😘")
