c26d73dde9f79df4c5a1e288c6b3e5193ea6f0e4
gamol4856-max <gamol4856@gmail.com>
2025-11-28 13:24:14 +0530

    escape sequence
    escape sequence of character in python

---
+++ escape sequence.py
@@ -0,0 +1,33 @@
+print ("Hello \nword")
+
+print ("Name:\tAlice \nAge: \t12 \nGrade: \t6")
+
+
+print ("it\'s a suuny day")
+
+print ("He said, \"Python is fun!\"")
+
+print("c:\\new_folder\\python")
+
+#Has a comment saying: # This program prints my name
+
+print ("AMOL")
+
+
+'''Multi-line comment
+
+Write a program that includes a multi-line comment explaining what the program does, then prints:
+'''
+
+print ("Python Comments Example")
+
+'''n = "3.99"
+print ('''#float(n)''', int(a)) # multiline comment in print statement'''
+
+n = "3.99"
+print (float(n))
+
+
+ #int(a)) single line comment in print statement
+       
+
