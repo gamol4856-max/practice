0330974347a1233bf5ef35267acb183302b48353
gamol4856-max <gamol4856@gmail.com>
2025-11-28 13:20:33 +0530

    Add files via upload
    typecasting of data type both implicit and explicit

---
+++ tyecasting.py
@@ -0,0 +1,44 @@
+a = "12"
+b = 12
+print (int(a) + (b))
+
+#explicit type casting
+
+a = 12
+b = 14
+print ("sum of and b is :", a + b)
+
+#implicit type casting
+
+#Beginner Type-Casting Problems
+
+#Convert the string "42" into an integer and print its type.
+
+a = "42"
+print (int,type(a))
+
+#Convert the integer 7 into a float.
+
+x = 7
+print (float, type(x))
+
+#Convert the float 3.14 into an integer.
+y = 3.14
+print (int, type(y))
+
+#Convert the string "3.99" into a float, then into an integer.
+
+n = "3.99"
+print (float(n), int(a))
+
+#Convert the boolean True into an integer.
+
+a = bool()
+
+print (int(a))
+
+# Convert the integer 0 into a boolean.
+
+a = bool(10)
+print (a)
+
