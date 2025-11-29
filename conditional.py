906d1476a1b92f197db9673ee36e1f1f99f5faa3
gamol4856-max <gamol4856@gmail.com>
2025-11-29 08:11:36 +0530

    conditional statement
    if else and for loop

---
+++ conditional.py
@@ -0,0 +1,35 @@
+'''number = 20
+age = number
+                                        #("variable swapped in if else statement")
+if age >= 18:
+    print ("your are adult")
+else:
+    print ("your are teenager")'''
+
+'''a = int(input("enter your number"))          #if else normal statement
+if a <= 0:
+    print ("negative number :", a)
+else:
+    print ("exit")'''
+
+
+'''print ("make sure you are out of 100")
+number = 18
+if number == 100:                                       #if else condition without input
+    print ("congratulations")
+else:
+    print ("take exit")'''
+
+
+#even odd numbers without input using for loop
+for num in range (20):
+    if num % 2 == 0:
+        print ("even number",num)
+    elif num % 2 == 1:
+        print ("odd number",num)
+
+
+
+
+
+
